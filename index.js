0f68919c2c5f768900b946a892fa623992dbab66
jli09 <56615820+jli09@users.noreply.github.com>
2020-08-21 19:05:53 -0400

    drew right leaf

--- index.js
+++ index.js
@@ -4,32 +4,40 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 });
 
 const c = canvas.getContext('2d');
 
 function drawGround() {
-    c.fillRect(0, innerHeight - innerHeight / 10, innerWidth, innerHeight / 10);
+  c.fillRect(0, innerHeight - innerHeight / 10, innerWidth, innerHeight / 10);
 }
 
 function drawPlant(width, height) {
-    const x = innerWidth / 2;
-    const y = (innerHeight - innerHeight / 10) - height;
+  const x = innerWidth / 2;
+  const y = innerHeight - innerHeight / 10 - height;
 
-    //draw trunk
-    c.fillRect(x, y, width, height);
+  //draw trunk
+  c.fillRect(x, y, width, height);
 
-    //draw leaves
+  //draw leaves
 
-    //left leaf
-    c.beginPath();
-    c.moveTo(x, y);
-    c.quadraticCurveTo(x - width, y - height / 3, x - width * 3, y - height / 5);
-    c.quadraticCurveTo(x - width * 2, y + height / 8, x, y);
-    c.stroke();
-    c.fill();
+  //left leaf
+  c.beginPath();
+  c.moveTo(x, y);
+  c.quadraticCurveTo(x - width, y - height / 3, x - width * 3, y - height / 5);
+  c.quadraticCurveTo(x - width * 2, y + height / 8, x, y);
+  c.stroke();
+  c.fill();
+
+  //right leaf
+  c.beginPath();
+  c.moveTo(x + width, y);
+  c.quadraticCurveTo(x + width * 2, y - height / 3, x + width * 4, y - height / 5);
+  c.quadraticCurveTo(x + width * 3, y + height / 8, x + width, y);
+  c.stroke();
+  c.fill();
 }
 
 drawGround();
