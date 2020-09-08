d723b221d1e382318cc5c843cfb38bbd6d760bcd
jli09 <56615820+jli09@users.noreply.github.com>
2020-09-08 15:02:42 -0400

    working on leaf stems

--- plant.js
+++ plant.js
@@ -17,40 +17,48 @@ class Plant {
     //draw trunk
     c.fillRect(x, y, this.width, this.height);
 
-    //draw left leaf
-      c.beginPath();
-      c.moveTo(x, y);
-      c.quadraticCurveTo(
-        x - this.width,
-        y - this.height / 3,
-        x - this.width * 3,
-        y - this.height / 5
-      );
-      c.quadraticCurveTo(x - this.width * 2, y + this.height / 8, x, y);
-      c.strokeStyle = '#00b100';
-      c.fillStyle = '#00b100';
-      c.stroke();
-      c.fill();
+    //draw left leaf w/ stem
+
+    //stem values:
+    const lsx = x - this.height / 10;
+    const lsy = y - this.height / 10;
+    const rsx = x + this.height / 10;
+    const rsy = y - this.height / 10;
+
+    c.beginPath();
+    c.moveTo(x, y);
+    c.lineTo(lsx, lsy);
+    c.quadraticCurveTo(
+      lsx - this.width,
+      lsy - this.height / 3,
+      lsx - this.width * 3,
+      lsy - this.height / 5
+    );
+    c.quadraticCurveTo(lsx - this.width * 2, lsy + this.height / 8, lsx, lsy);
+    c.strokeStyle = '#00b100';
+    c.fillStyle = '#00b100';
+    c.stroke();
+    c.fill();
 
     //draw right leaf
-      c.beginPath();
-      c.moveTo(x + this.width, y);
-      c.quadraticCurveTo(
-        x + this.width * 2,
-        y - this.height / 3,
-        x + this.width * 4,
-        y - this.height / 5
-      );
-      c.quadraticCurveTo(
-        x + this.width * 3,
-        y + this.height / 8,
-        x + this.width,
-        y
-      );
-      c.strokeStyle = '#00b100';
-      c.fillStyle = '#00b100';
-      c.stroke();
-      c.fill();
+    c.beginPath();
+    c.moveTo(x + this.width, y);
+    c.quadraticCurveTo(
+      rsx + this.width * 2,
+      rsy - this.height / 3,
+      rsx + this.width * 4,
+      rsy - this.height / 5
+    );
+    c.quadraticCurveTo(
+      rsx + this.width * 3,
+      rsy + this.height / 8,
+      rsx + this.width,
+      rsy
+    );
+    c.strokeStyle = '#00b100';
+    c.fillStyle = '#00b100';
+    c.stroke();
+    c.fill();
   }
 
   grow() {
@@ -61,7 +69,7 @@ class Plant {
     if (this.height <= this.maxHeight) {
       this.height++;
     } else {
-        this.grown = true;
-      }
+      this.grown = true;
     }
+  }
 }
