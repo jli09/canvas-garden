085b8bdb8f3d585d2278be13fb2fd14f35e72739
jli09 <56615820+jli09@users.noreply.github.com>
2020-09-11 18:30:24 -0400

    added leaves to Branch class

--- branch.js
+++ branch.js
@@ -24,7 +24,8 @@ class Branch {
     }
 
     if (this.width > this.root.width * -1) {
-      this.width += (this.root.maxWidth / this.root.maxHeight) * .75 * this.rateMod;
+      this.width +=
+        (this.root.maxWidth / this.root.maxHeight) * 0.75 * this.rateMod;
     }
   }
 
@@ -47,6 +48,50 @@ class Branch {
     c.lineTo(startX, startY + this.startYChange + this.width);
     c.strokeStyle = '#006400';
     c.fillStyle = '#006400';
+    c.stroke();
+    c.fill();
+
+    //draw leaf
+    let cpx = startX + this.changeX * 1.25;
+    let cpy = startY + this.startYChange + this.changeY * 2;
+
+    let cpx2 = startX + this.changeX * 1.5;
+    let cpy2 = startY + this.startYChange + this.width + this.changeY * 0.75;
+
+    //-- for tracking purposes --
+    // c.beginPath();
+    // c.moveTo(cpx, cpy);
+    // c.arc(cpx, cpy, 2, 0, Math.PI * 2);
+    // c.strokeStyle = 'red';
+    // c.stroke();
+    // c.fillStyle = 'red';
+    // c.fill();
+
+    // c.beginPath();
+    // c.moveTo(cpx2, cpy2);
+    // c.arc(cpx2, cpy2, 2, 0, Math.PI * 2);
+    // c.strokeStyle = 'blue';
+    // c.stroke();
+    // c.fillStyle = 'blue';
+    // c.fill();
+
+    c.beginPath();
+    c.moveTo(startX + this.changeX, startY + this.startYChange + this.changeY);
+    c.quadraticCurveTo(
+      cpx,
+      cpy,
+      startX + this.changeX * 1.8,
+      startY + this.startYChange + this.changeY * 1.5
+    );
+    c.quadraticCurveTo(
+      cpx2,
+      cpy2,
+      startX + this.changeX,
+      startY + this.startYChange + this.changeY + this.width
+    );
+    c.lineTo(startX + this.changeX, startY + this.startYChange + this.changeY);
+    c.strokeStyle = '#00b100';
+    c.fillStyle = '#00b100';
     c.stroke();
     c.fill();
   }
